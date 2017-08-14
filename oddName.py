8613df3e6bc37eabd23345c5b1207f4baf8add5b
jeanpaulpitman <jeanpaul.pitman@my.jcu.edu.au>
2017-08-14 16:47:33 +1000

    add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,11 @@
 """Jean-Paul Pitman."""
+
+
+def main():
+    user_name = ""
+    while user_name == "":
+        print("Please enter your name")
+        user_name = input("Name: ")
+    print(user_name[::2])
+
+main()
