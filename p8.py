5a8e88c522c43eff583929ffce80ad3e559eff46
jeanpaulpitman <jeanpaul.pitman@my.jcu.edu.au>
2017-11-01 14:24:58 +1000

    :owl:  exam practice questions complete

---
+++ p8.py
@@ -0,0 +1,5 @@
+s = "Welcome to the Jungle"
+new_s = ""
+for letter in s:
+    new_s += (letter * 2)
+print(new_s)
--- q7.py
+++ q7.py
@@ -3,12 +3,14 @@ from random import randint
 
 
 def get_random_numbers(number):
-    random_numbers = set()
-    while len(random_numbers) < number:
-        random_number = randint(1, (number * 10) + 1)
-        random_numbers.add(random_number)
-    return list(random_numbers)
+    unique_numbers = set()
+    while len(unique_numbers) < number:
+        random_number = randint(1, (number * 10))
+        unique_numbers.add(random_number)
+    random_numbers = list(unique_numbers)
+    random_numbers.sort(reverse=True)
+    return random_numbers
 
 
-number_list = get_random_numbers(5)
+number_list = get_random_numbers(50)
 print(number_list)
